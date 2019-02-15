efde174b1a95e7d11e1d3ff8791965f8129db8c9
Roberto Rosario <Roberto.Rosario@mayan-edms.com>
2019-02-15 04:16:30 -0400

    Add MayanAPIGenericViewSet
    This viewset allow adding generic action API endpoints.
    
    Signed-off-by: Roberto Rosario <Roberto.Rosario@mayan-edms.com>

--- mayan/apps/rest_api/viewsets.py
+++ mayan/apps/rest_api/viewsets.py
@@ -1,9 +1,21 @@
 from __future__ import absolute_import, unicode_literals
 
 from rest_framework import viewsets
+from rest_framework.settings import api_settings
 
 from .filters import MayanViewSetObjectPermissionsFilter
 from .permissions import MayanViewSetPermission
+
+
+class MayanAPIGenericViewSet(viewsets.GenericViewSet):
+    filter_backends = (MayanViewSetObjectPermissionsFilter,)
+    permission_classes = (MayanViewSetPermission,)
+
+    def get_success_headers(self, data):
+        try:
+            return {'Location': str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {}
 
 
 class MayanAPIModelViewSet(viewsets.ModelViewSet):
