f0ae0d06c7cf92844e556fe90753b6cc26d81dad
Roberto Rosario <Roberto.Rosario@mayan-edms.com>
2019-07-02 21:40:12 -0400

    Switch to 2 Redis databases
    Signed-off-by: Roberto Rosario <Roberto.Rosario@mayan-edms.com>

--- mayan/apps/platform/classes.py
+++ mayan/apps/platform/classes.py
@@ -108,7 +108,7 @@ class PlatformTemplate(object):
 class PlatformTemplateSupervisord(PlatformTemplate):
     context_defaults = {
         'BROKER_URL': 'redis://127.0.0.1:6379/0',
-        'CELERY_RESULT_BACKEND': 'redis://127.0.0.1:6379/0',
+        'CELERY_RESULT_BACKEND': 'redis://127.0.0.1:6379/1',
     }
     label = _('Template for Supervisord.')
     name = 'supervisord'
